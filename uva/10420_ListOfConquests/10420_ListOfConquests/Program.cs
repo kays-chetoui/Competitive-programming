7d3f493c792a26232a0cbce6ef9c83986eec1bbd
Kays Chetoui <kayschetoui.perso@gmail.com>
2019-10-29 15:05:41 +0100

    add 10420_ListOfConquests

---
+++ uva/10420_ListOfConquests/10420_ListOfConquests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10420_ListOfConquests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, int>();
+            for (int i = 0; i < n; i++)
+            {
+                var str = Console.ReadLine();
+                var country = str.Substring(0, str.IndexOf(" "));
+                if (dict.ContainsKey(country))
+                {
+                    dict[country]++;
+                }
+                else
+                {
+                    dict.Add(country, 1);
+                }
+            }
+            
+            Console.WriteLine(string.Join(Environment.NewLine, dict.OrderBy(e => e.Key).Select(e => e.Key + " " + e.Value)));
+        }
+    }
+}
